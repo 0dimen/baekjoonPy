d6c3c2bd3dca9e18447ac3862a7f2fd5e29741fd
0dimen <first11.konkuk.ac.kr>
2022-11-06 15:26:13 +0900

    별 찍기 : https://www.acmicpc.net/problem/2438 for문 이용 68ms

---
+++ 2211060.py
@@ -0,0 +1,12 @@
+# 별 찍기 : https://www.acmicpc.net/problem/2438
+
+def writeStar(N):
+    for lineNum in range (N):
+        output = ""
+        for repeatNum in range (0, lineNum+1):
+            output += "*"
+        print(output)
+
+if __name__ == '__main__':
+    writeStar(int(input()))
+
